5d79c50f26de81cce8d7df9f2ae3929fec643b01
dewei.ma <dewei.ma@lvyuetravel.com>
2021-04-19 08:38:17 +0800

    chore: tools setup

---
+++ install_tools.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+readonly TOOL_PATH="$HOME/.myprofile/tools"
+
+lnif() {
+    if [ -e "$1" ]; then
+        ln -sf "$1" "$2"
+    fi
+}
+
+createSymlinks() {
+  local index=1
+
+  for linkName in "$@"
+  do
+    lnif "$TOOL_PATH/$linkName" "/usr/local/bin/${linkName%.sh}"
+  done
+}
+
+createSymlinks $(ls "${TOOL_PATH}")
