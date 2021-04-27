b07135ea5a77454cd1fb27586355a4d430d3157a
dewei.ma <dewei.ma@lvyuetravel.com>
2021-04-27 10:33:08 +0800

    chore: :up: update init env

--- init_env.sh
+++ init_env.sh
@@ -15,24 +15,10 @@ runCommand() {
     eval "$cmd" install "$other_args"
 }
 
-brewSource() {
-    cmd=$1
-    args=$*
-    other_args=${args#* }
-
-    eval brew install --build-from-source "$other_args"
-}
-
 hasCommand node || ./setup/nvm-install.sh
 hasCommand pip || python ./setup/get-pip.py
 hasCommand brew || ./setup/homebrew_cn_install.sh
-hasCommand pip && pip install --user pynvim neovim
 
-# brewSource shellcheck
-
-runCommand pip yapf \
-    isort flake8 pylint autopep8 vim-vint
-
-runCommand brew the_silver_searcher \
+runCommand brew the_silver_searcher make cmake \
     ctags ccls uncrustify tidy-html5 yamllint \
     shfmt swiftformat swiftlint shellcheck
