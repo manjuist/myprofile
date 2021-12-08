4526f9f8a8035d9d2c2c18a9cd5c5c9dbda505ca
dewei.ma <mdeve@qq.com>
2021-12-08 22:32:52 +0800

    feat: hyper

--- config/hyper.js
+++ config/hyper.js
@@ -135,7 +135,7 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: [],
+    plugins: ["hyperpower", "hyper-gruvbox"],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
