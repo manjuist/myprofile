c1f640006fb3760a50c47ab15af05a3543c306b7
dewei.ma <mdeve@qq.com>
2022-03-31 08:14:12 +0800

    fix: hyper conf

--- config/hyper.js
+++ config/hyper.js
@@ -97,7 +97,7 @@ module.exports = {
         // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
         // by default `['--login']` will be used
         //shellArgs: ['--login'],
-        shellArgs: [],
+        //shellArgs: [],
         // for environment variables
         env: {},
         // Supported Options:
