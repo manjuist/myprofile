3738972777f7908ecfe7762b746f9a60e673a0ee
dewei.ma <xnmageis@outlook.com>
2023-02-17 14:30:36 +0800

    feat: del package

--- setup-brew.sh
+++ setup-brew.sh
@@ -14,12 +14,12 @@ brew install tmux ripgrep shellcheck shfmt the_silver_searcher tidy-html5 dust \
     nvm btop neofetch zoxide aria2 cmake neovim swiftlint swiftformat exa make \
     graphicsmagick highlight libxml2 zsh ctags openssl bat rust-analyzer
 
-brew install --cask blender macdown monitorcontrol rocket-chat calibre firefox \
+brew install --cask macdown rocket-chat calibre firefox v2rayu he3-app/he3/he3 \
     youdaodict krita scribus rectangle sigil dbeaver-community kitty stats xld \
     losslesscut shotcut emacs flameshot macpass keepassxc wireshark foobar2000 \
     hiddenbar itsycal keka kekaexternalhelper lulu netnewswire httpie obsidian \
     free-download-manager alt-tab nutstore syntax-highlight cyberduck kdenlive \
     meld tencent-lemon thor visual-studio-code charles drawio alacritty pencil \
     wechat handbrake docker opentoonz robo-3t audacity skim clipy inkscape mpv \
-    another-redis-desktop-manager google-chrome gas-mask gimp v2rayu go2shell \
-    he3-app/he3/he3 goldendict neovide
+    another-redis-desktop-manager google-chrome gas-mask gimp blender go2shell \
+    goldendict neovide
