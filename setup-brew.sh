1a06deaabafb63cc4491823dc48baed225003047
dewei.ma <xnmageis@outlook.com>
2024-01-30 02:23:35 +0800

    chore: packages

--- setup-brew.sh
+++ setup-brew.sh
@@ -16,21 +16,19 @@ brew tap macos-fuse-t/homebrew-cask
 brew install fuse-t
 brew install fuse-t-sshfs
 
-brew install fd exa duf fzf gcc gpg nnn nvm zsh bat dust btop make tmux cowsay \
+brew install fd fzf gcc gpg nnn nvm zsh btop isort pylint autopep8 swiftformat \
     openssl neofetch yamllint swiftlint highlight git-delta shellcheck lazygit \
-    tidy-html5 difftastic swiftformat rust-analyzer the_silver_searcher scrcpy \
-    cmake ctags xonsh zoxide neovim ffmpeg pandoc ripgrep libxml2 shfmt ranger \
-    jsonlint prettier stylelint yapf isort flake8 pylint autopep8 thefuck tldr \
-    node
+    cmake ctags xonsh zoxide neovim ffmpeg pandoc ripgrep libxml2 shfmt scrcpy \
+    jsonlint prettier stylelint yapf flake8 node tmux tidy-html5 rust-analyzer \
+    the_silver_searcher make
 
-brew install --cask docker blender lulu gimp thor keka skim meld espanso olive \
-    motrix findergo flameshot rectangle hiddenbar inkscape fontforge stretchly \
-    robo-3t alt-tab nutstore obsidian audacity caffeine iina syncthing itsycal \
+brew install --cask docker blender lulu gimp thor keka skim espanso olive gitx \
+    motrix findergo flameshot rectangle inkscape fontforge stretchly freeplane \
+    robo-3t alt-tab nutstore obsidian audacity iina syncthing itsycal squirrel \
     keepassxc pencil alacritty kitty firefox macpass macdown dbeaver-community \
     visual-studio-code kekaexternalhelper another-redis-desktop-manager drawio \
     intellij-idea-ce syntax-highlight microsoft-onenote goldendict httpie pika \
-    cryptomator balenaetcher mongodb-compass switchhosts adapter google-chrome \
-    netnewswire thunderbird musicbrainz-picard textmate squirrel kap freeplane \
-    calibre duplicati zotero clipy gitx
+    cryptomator mongodb-compass switchhosts adapter google-chrome textmate kap \
+    calibre zotero clipy keepingyouawake netnewswire musicbrainz-picard
 
-# xld wireshark kdenlive kid3 charles freac handbrake
+# xld wireshark kid3 charles freac handbrake
