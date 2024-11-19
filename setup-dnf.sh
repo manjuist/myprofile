8ce9460a6ad3e59d0a968623a5cd10ee72db7237
dewei.ma <xnmageis@outlook.com>
2024-11-19 18:33:42 +0800

    feat: packages

--- setup-dnf.sh
+++ setup-dnf.sh
@@ -7,30 +7,35 @@
 # ------
 
 # another-redis-desktop-manager
+# bottles
 # clash-verge-rev
 # lazygit
 # mkcert
 # nekoray
 # robo3t
 # scrcpy
+# simplescreenrecorder
 # switchhosts
 
 set -e
 set -o pipefail
 
-sudo dnf install -y aria2 audacity blender btop bottles calibre cmake copyq ctags \
-    docker fcitx5 fcitx5-chinese-addons fcitx5-configtool fd-find feh ffmpeg-free \
-    ffmpegthumbs filezilla flameshot fzf gcc gimp git git-cola gnupg goldendict \
-    guake highlight httpie i3 i3lock i3status inkscape jq keepass \ keepassxc \
-    kitty libxml2 make mousepad neovim nnn nodejs npm openssl p7zip pandoc \
-    pdfarranger picard picom poppler remmina ripgrep rofi shellcheck shfmt \
-    simplescreenrecorder shotcut thunderbird tilix tmux vifm vlc wireshark yamllint \
-    yarnpkg zoxide zsh GraphicsMagick ImageMagick fcitx5-qt6 the_silver_searcher \
-    nss-tools hadolint rust-analyzer networkmanager rxvt-unicode azote dunst \
-    htop thunar
+rpm -Uvh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-stable.noarch.rpm
 
-flatpak install flathub md.obsidian.Obsidian com.github.zadam.trilium \
-    com.visualstudio.code com.jgraph.drawio.desktop org.localsend.localsend_app \
-    com.google.Chrome net.hovancik.Stretchly org.freac.freac net.agalwood.Motrix \
-    com.wps.Office org.cryptomator.Cryptomator io.dbeaver.DBeaverCommunity \
-    com.tencent.WeChat org.freeplane.App org.freefilesync.FreeFileSync
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+sudo dnf install -y aria2 audacity blender btop calibre cmake copyq ctags docker \
+    fcitx5 fcitx5-chinese-addons fcitx5-configtool fd-find feh ffmpeg-free ffmpegthumbs \
+    filezilla flameshot fzf gcc gimp git git-cola gnupg goldendict guake highlight \
+    httpie i3 i3lock i3status inkscape jq keepass keepassxc kitty libxml2 make \
+    mousepad neovim nnn nodejs npm openssl p7zip pandoc pdfarranger picard picom \
+    poppler remmina ripgrep rofi shellcheck shfmt shotcut thunderbird tilix tmux \
+    vifm vlc wireshark yamllint yarnpkg zoxide zsh GraphicsMagick ImageMagick fcitx5-qt6 \
+    the_silver_searcher nss-tools hadolint rust-analyzer NetworkManager rxvt-unicode \
+    azote dunst htop thunar
+
+flatpak install flathub md.obsidian.Obsidian com.github.zadam.trilium com.visualstudio.code \
+    com.jgraph.drawio.desktop org.localsend.localsend_app com.google.Chrome net.hovancik.Stretchly \
+    org.freac.freac net.agalwood.Motrix com.wps.Office org.cryptomator.Cryptomator \
+    io.dbeaver.DBeaverCommunity com.tencent.WeChat org.freeplane.App org.freefilesync.FreeFileSync
