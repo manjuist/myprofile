a086acf17f29abca64904c1e050e47d952aba60a
dewei.ma <xnmageis@outlook.com>
2024-11-23 23:05:15 +0800

    feat: rust

--- install-pre.sh
+++ install-pre.sh
@@ -10,7 +10,7 @@ set -e
 # set -u
 set -o pipefail
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 curl -LsSf https://astral.sh/uv/install.sh | sh || error "Failed to install uv"
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh || error "Failed to install zim"
 curl -sS https://starship.rs/install.sh | sh || error "Failed to install starship"
