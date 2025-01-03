1ec739ed55a27815ba0130a1b72efea3d5621aa9
dewei.ma <xnmageis@outlook.com>
2025-01-03 18:37:14 +0800

    feat: tty theme

--- config_files/wezterm.lua
+++ config_files/wezterm.lua
@@ -1,16 +1,25 @@
 local wezterm = require("wezterm")
 
 return {
+    -- color_scheme = "carbonfox"
+    -- color_scheme = "Catppuccin Mocha"
+    -- color_scheme = "duskfox"
+    -- color_scheme = "GruvboxDark"
+    -- color_scheme = "GruvboxDarkHard"
+    -- color_scheme = "Kanagawa Dragon (Gogh)"
+    -- color_scheme = "nightfox"
     color_scheme = "terafox",
+    -- color_scheme = "tokyonight"
 
     font = wezterm.font_with_fallback({
+        -- "SauceCodePro Nerd Font",
         "CartographCF Nerd Font Mono",
         "Source Han Sans SC",
     }),
 
     font_size = 14.0,
 
-    enable_tab_bar = false,
-
     default_prog = { "/Users/mdvis/.local/bin/tmux" },
+
+    enable_tab_bar = false,
 }
