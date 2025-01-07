ec7868662dd520234f9e2e5c15f5dffcbb520411
dewei.ma <xnmageis@outlook.com>
2025-01-07 16:33:27 +0800

    feat: apostrophe

--- setup-dnf.sh
+++ setup-dnf.sh
@@ -24,7 +24,7 @@ sudo dnf install -y aria2 audacity blender btop calibre cmake copyq ctags docker
     shotcut thunderbird tilix tmux vifm vlc wireshark yamllint yarnpkg zoxide zsh \
     GraphicsMagick ImageMagick the_silver_searcher nss-tools hadolint rust-analyzer \
     NetworkManager rxvt-unicode azote dunst htop thunar snapd hyprland hyprland-devel \
-    fio hdparm wabt wike secrets foliate apostrophe sway ghostty foot
+    fio hdparm wabt wike secrets foliate sway ghostty foot
 
 # fcitx5
 sudo dnf install -y imsettings fcitx5-table-extra fcitx5 fcitx5-chinese-addons \
@@ -41,7 +41,7 @@ flatpak install flathub md.obsidian.Obsidian com.visualstudio.code com.jgraph.dr
     org.localsend.localsend_app net.hovancik.Stretchly org.freac.freac net.agalwood.Motrix \
     org.cryptomator.Cryptomator io.dbeaver.DBeaverCommunity org.freefilesync.FreeFileSync \
     org.freeplane.App org.freecad.FreeCAD com.tencent.WeChat io.github.alainm23.planify \
-    io.gitlab.news_flash.NewsFlash org.wezfurlong.wezterm
+    io.gitlab.news_flash.NewsFlash org.wezfurlong.wezterm org.gnome.gitlab.somas.Apostrophe
 
 if [ "$ARCH" = "$AMD64" ]; then
     flatpak install flathub com.usebottles.bottles com.github.zadam.trilium com.google.Chrome \
